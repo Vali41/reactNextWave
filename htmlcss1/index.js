6b22778eb6007feb8344aa524bf6ec14a4510bae
shavali <shaiksha.vali41@gmail.com>
2025-02-26 15:43:44 +0530

    childElement added

--- htmlcss1/index.js
+++ htmlcss1/index.js
@@ -1,5 +1,15 @@
-const btnElement= document.getElementById('click-btn')
+let btnElement= document.getElementById('click-btn');
+let counterElement=document.getElementById('counter-container');
 
 btnElement.addEventListener('click',e=>{
 	console.log('Event triged')
 })
+
+const timer= document.createElement('p')
+
+const date= new Date()
+const time= date.toLocaleTimeString ()
+
+timer.textContent=`current time is : ${time}`
+
+counterElement.appendChild(timer)
